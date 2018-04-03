964086c63150c9580de841b79110bfceb5931f46
Efrat Rosmarin <efratedit@gmail.com>
2018-04-03 12:23:40 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+console.log(process.env.SPOTIFY_CLIENTID)
+console.log(process.env.SPOTIFY_CLIENTSECRET)
+console.log(process.env.TWITTERKEY)
+console.log(process.env.TWITTERID)
